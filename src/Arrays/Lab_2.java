debf6d8b225c1ea399a1b3c737f16e52b7601b61
suada <suadaljuaid1@gmail.com>
2025-11-12 11:29:47 +0300

    edit print message

--- src/Arrays/Lab_2.java
+++ src/Arrays/Lab_2.java
@@ -3,7 +3,7 @@ package Arrays;
 public class Lab_2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world2");
 
     }
 }
