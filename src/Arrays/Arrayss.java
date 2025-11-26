b8f5ee42b4adbb0ec5905b0cb06d97b0310bee0f
suada <suadaljuaid1@gmail.com>
2025-11-26 19:39:18 +0300

    Lab3- Arrays -Part2

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -14,9 +14,49 @@ public class Arrayss {
 
     }
 
+    public void Traversal2(int []a){
+                for(int e:a)
+            System.out.println(e);
+
+    }
+
     public void input(int[]a){
         System.out.println("enter "+a.length+" elements:");
         for (int i=0; i<a.length;i++)
             a[i]=scanner.nextInt();
     }
+
+
+    public void update(int[]a, int index, int nvalue){
+        if(index<0 || index>=a.length)
+            System.out.println("Index not found");
+        else
+        a[index]=nvalue;
+
+    }
+
+    public void delete(int[]a, int index){
+        if(index<0 || index>=a.length)
+            System.out.println("Index not found");
+        else
+            a[index]=0;
+
+    }
+
+    public void shiftLeft_Delete(int[]a,int index){
+        for(int i=index; i<a.length-1;i++)
+            a[i]=a[i+1];
+
+        a[a.length-1]=Integer.MIN_VALUE;
+    }
+
+
+public int LinearSearch(int []a, int svalue){
+   for (int i=0; i<a.length;i++)
+   {
+       if(svalue==a[i])
+           return i;
+   }
+        return -1;
+}
 }
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -6,15 +6,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Arrayss arr=new Arrayss();
-       // int []numbers={1,2,3,4,5};
-        int []numbers=new int[7];
+        int []numbers={1,2,3,4,5};
+        int result=arr.LinearSearch(numbers,2);
+
+        if(result!=-1)
+        System.out.println("value found at index :"+result);
+        else
+        System.out.println("Value not found");
+      //  numbers[2]=7;
+        //int []numbers=new int[7];
+
+        //
+        //arr.delete(numbers,2);
+///arr.shiftLeft_Delete(numbers,2);
+        // arr.input(numbers);
+       // arr.Traversal(numbers);
 
 
-        arr.input(numbers);
-        arr.Traversal(numbers);
-
-//        for(int number:numbers)
-//            System.out.println(number);
 
         //System.out.println(Arrays.toString(numbers));
 
